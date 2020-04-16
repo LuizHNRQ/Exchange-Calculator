fa61a20ae539071e7b6b78d0bcfbeb7a28b93597
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2020-04-16 18:06:42 -0300

    feat: added fetch to the API

---
+++ script.js
@@ -0,0 +1,32 @@
+const currency1 = document.querySelector('#currency-one');
+const amount1 = document.querySelector('#amount-one');
+const currency2 = document.querySelector('#currency-two');
+const amount2 = document.querySelector('#amount-two');
+
+const rateElement = document.querySelector('#rate');
+const swap = document.querySelector('#swap');
+
+//Featch echange rate and update the DOM
+function calculate() {
+  const cur_one = currency1.value;
+  const cur_two = currency2.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${cur_one}`)
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
+      const rate = data.rates[cur_two];
+      console.log(rate);
+
+      rateElement.innerText = `1 ${cur_one} = ${rate} ${cur_two}`;
+
+    });
+};
+
+
+//Event Listners
+
+currency1.addEventListener('change', calculate);
+amount1.addEventListener('input', calculate);
+currency2.addEventListener('change', calculate);
+amount2.addEventListener('input', calculate);
